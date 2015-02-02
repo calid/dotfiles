558ea39f3ce469d0ad207b305e709ba2022d2f30
Dylan Cali <calid1984@gmail.com>
2015-02-02 03:31:16 -0600

    pipe colordiff to less
    auto-handles paging when appropriate, and LESS options are set such that
    color will be preserved by default

--- bashrc.d/aliases.sh
+++ bashrc.d/aliases.sh
@@ -3,7 +3,10 @@ alias gits='git status -s'
 alias ls='ls --color=auto'
 alias cpanm='cpanm -v'
 alias tree='tree -aC -I .git'
-alias diff='colordiff -u'
 alias view='vim -R'
 alias gview='gvim -R'
 alias vi='vim -v -u NONE'
+
+function diff {
+    colordiff -u $@ | less
+}
