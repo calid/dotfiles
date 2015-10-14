340b2d613fcf5d830ceb30940787a22dfac7ac91
Dylan Cali <calid1984@gmail.com>
2015-10-13 23:57:02 -0400

    unset PERL5LIB to ensure clean state

--- profile.d/perl.sh
+++ profile.d/perl.sh
@@ -1,5 +1,8 @@
 export PERL_LOCAL_LIB_ROOT="$HOME/perl5";
 export PERL_MB_OPT="--install_base $HOME/perl5";
 export PERL_MM_OPT="INSTALL_BASE=$HOME/perl5";
+
+unset PERL5LIB
 export PERL5LIB="$HOME/perl5/lib/perl5/x86_64-linux-gnu-thread-multi:$HOME/perl5/lib/perl5";
+
 export PATH="$HOME/perl5/bin:$PATH";
