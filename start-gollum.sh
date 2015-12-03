7909ed36d6ea9deebdc34b2724c71765279bbd89
Dylan Cali <calid1984@gmail.com>
2015-12-03 13:16:22 -0500

    parameterize start gollum script

--- start-gollum.sh
+++ start-gollum.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+if [ $# -ne 3 ]
+then
+    echo "Usage: $(basename $0) <wiki-root> <host> <port>"
+    exit 1
+fi
+
 wiki_root="$1"
+host="$2"
+port="$3"
 
 gollum \
-    --host $(hostname) --port 8080 \
+    --host $host --port $port \
     --adapter rugged \
     --show-all \
     --mathjax \
